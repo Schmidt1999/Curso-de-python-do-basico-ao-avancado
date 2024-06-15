cd09ad9756ba396d75205837b8b3091ef59b2e6f
Schmidt1999 <matheusschmidtf@protonmail.com>
2024-06-15 02:17:24 -0300

    Aula 29

---
+++ aula29.py
@@ -0,0 +1,25 @@
+"""
+--- Introdução ao try/except ---
+try -> tentar executar o código
+except -> ocorreu algum erro ao executar
+"""
+
+numero_str = input('vou dobrar o numero que voce digitar: ')
+
+try:
+    print('STR: ', numero_str)
+    numero_float = float(numero_str)
+    print('FLOAT: ', numero_float)
+    print(f'O dobro de {numero_float} é {numero_float * 2:.1f}')
+except:
+    print("Isso nào é um digito")
+
+
+
+# if numero_str.isdigit():
+#     
+#  else:
+#     
+
+
+
