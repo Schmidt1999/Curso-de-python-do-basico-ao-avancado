867e15aab5d9ffbcb80c2d89df77258339fa84b4
Schmidt1999 <matheusschmidtf@protonmail.com>
2024-06-15 13:48:54 -0300

    Aula 31

--- aula31.py
+++ aula31.py
@@ -4,15 +4,26 @@ none = nao valor
 is e is not = é ou nao é (tipo, valor, identidade)
 id = identidade
 """
-v1 = 'a'
-v2 = 'a'
-v3 = 'b'
-print(id(v1))
-print(id(v2))
-print(id(v3))
+# v1 = 'a'
+# v2 = 'a'
+# v3 = 'b'
+# print(id(v1))
+# print(id(v2))
+# print(id(v3))
 
-# condicao = False
-# if condicao:
-#     print('Faça algo')
-# else:
-#     print('Não faça algo')
+condicao = True
+passou_no_if = None
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+print(passou_no_if, passou_no_if is None)
+print(passou_no_if, passou_no_if is not None)
+
+if passou_no_if is None:
+    print('Não passou no if')
+else:
+    print('Passou no if')
+    
