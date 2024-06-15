771976e4cda27c36fd9650ac00e793d9594e4a1f
Schmidt1999 <matheusschmidtf@protonmail.com>
2024-06-15 13:38:15 -0300

    Aula 31

---
+++ aula31.py
@@ -0,0 +1,18 @@
+"""
+flag - marcar um local
+none = nao valor
+is e is not = é ou nao é (tipo, valor, identidade)
+id = identidade
+"""
+v1 = 'a'
+v2 = 'a'
+v3 = 'b'
+print(id(v1))
+print(id(v2))
+print(id(v3))
+
+# condicao = False
+# if condicao:
+#     print('Faça algo')
+# else:
+#     print('Não faça algo')
