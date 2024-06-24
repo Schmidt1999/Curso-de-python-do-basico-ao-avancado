216c05a97781210c0e1fdb8b60e16c018593de0d
Schmidt1999 <matheusschmidtf@protonmail.com>
2024-06-24 02:27:17 -0300

    Aula 37 while loops

--- aula37.py
+++ aula37.py
@@ -5,8 +5,17 @@ Executa uma ação enquanto uma condição for verdadeira
 """
 contador = 0
 
-while contador <= 1000000:
+while contador <= 1000:
     contador += 1
+
+    if contador == 6:
+        print('Não vou mostrar o 6')
+        continue
+
+    if contador >= 10 and contador < 20:
+        print('Não vou mostrar o', contador)
+        continue
+
     print(contador)
 
     if contador == 400:
