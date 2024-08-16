eda32e4849e9d20d186d9fe0d5ed6cf53dd5fb65
Schmidt1999 <matheusschmidtf@protonmail.com>
2024-08-16 00:40:46 -0300

    Aula 43 estrutura de repeticao em loops finitos

---
+++ aula43.py
@@ -0,0 +1,16 @@
+# senha_salva = '12345'
+# senha_digitada = ''
+# repeticoes = 0
+
+# while senha_salva != senha_digitada:
+#     senha_digitada = input(f'Sua senha ({repeticoes}x): ')
+#     repeticoes += 1
+
+# print(repeticoes)
+
+texto = "matheuslindao"
+
+for i in texto: 
+    int(i)
+    i += 1
+print(i)
